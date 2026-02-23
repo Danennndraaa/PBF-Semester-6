2a3be2d777d3eeb534a72a72ad55e5326811d65b
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2026-02-24 00:49:54 +0700

    E. Tugas Praktikum - Tugas 3

--- Praktikum 4 - Link Navigation/src/pages/produk/index.tsx
+++ Praktikum 4 - Link Navigation/src/pages/produk/index.tsx
@@ -2,15 +2,16 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const produk = () => {
-  // const [isLogin, setIsLogin] = useState(false);
-  // const { push } = useRouter();
-  // useEffect(() => {
-  //   if (!isLogin) {
-  //     push("/auth/login");
-  //   }
-  // }, [isLogin, push]);
+  const [isLogin, setIsLogin] = useState(false); 
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (!isLogin) {
+      push("/auth/login");
+    }
+  }, []);
   return (
-    <div>
+    <div style={{ padding: '20px' }}>
         Produk User Page
     </div>
   );
