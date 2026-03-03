e4cd254169a8bba64488211b804e025d9dcf58c4
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2026-03-03 23:47:23 +0700

    Bagian 5 – Implementasi SWR

--- Praktikum 8 - Client Side Rendering/src/pages/produk/index.tsx
+++ Praktikum 8 - Client Side Rendering/src/pages/produk/index.tsx
@@ -1,37 +1,55 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import TampilanProduk from "../views/product";
+import useSWR from "swr";
+import fetcher from "../utils/swr/fetcher";
+
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Kategori = () => {
-  // Menambahkan tipe data ke useState agar TypeScript lebih akurat
   const [products, setProduct] = useState([]);
 
-  // 1. Ekstrak logika fetch ke dalam fungsi terpisah
-  const fetchData = () => {
-    fetch("/api/produk")
-      .then((response) => response.json())
-      .then((responsedata) => {
-        // console.log(responsedata.data);
-        setProduct(responsedata.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching products:", error);
-      });
-  };
-
-  // 2. Panggil fetchData saat komponen pertama kali di-mount
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
 
   return (
     <div>
-      <TampilanProduk products={products} />
+      <TampilanProduk products={isLoading ? [] : data.data} /> 
     </div>
   );
 };
 
 export default Kategori;
+
+// const Kategori = () => {
+//   // Menambahkan tipe data ke useState agar TypeScript lebih akurat
+//   const [products, setProduct] = useState([]);
+
+//   // 1. Ekstrak logika fetch ke dalam fungsi terpisah
+//   const fetchData = () => {
+//     fetch("/api/produk")
+//       .then((response) => response.json())
+//       .then((responsedata) => {
+//         // console.log(responsedata.data);
+//         setProduct(responsedata.data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching products:", error);
+//       });
+//   };
+
+//   // 2. Panggil fetchData saat komponen pertama kali di-mount
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div>
+//       <TampilanProduk products={products} />
+//     </div>
+//   );
+// };
+
+// export default Kategori;
 
 
 
