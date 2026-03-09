d5e3c0d390efd5a2c79172cc4a73452318e77faf
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2026-03-09 22:36:55 +0700

    Bagian 1 – Setup Halaman SSR

--- Praktikum 9 - Server Side Rendering/src/components/layouts/Appshell/index.tsx
+++ Praktikum 9 - Server Side Rendering/src/components/layouts/Appshell/index.tsx
@@ -14,9 +14,9 @@ const Appshell = (props:AppshellProps) => {
     <main>
       {!disableNavbar.includes(pathname) && <Navbar />}
       {children}
-      <footer style={{ backgroundColor: '#333', color: 'white', padding: '15px', textAlign: 'center' }}>
+      {/* <footer style={{ backgroundColor: '#333', color: 'white', padding: '15px', textAlign: 'center' }}>
         <p>&copy; Danendra - 244107023011 - 2026 - Pemrograman Berbasis Framework</p>
-      </footer>
+      </footer> */}
     </main>
   );
 };
