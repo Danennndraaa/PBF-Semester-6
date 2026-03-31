aab2819bc0af0b71fba405c7068ed3afacb722cf
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2026-03-31 23:45:40 +0700

    E. Proteksi Halaman Profile
    Buat Halaman Profile

--- Praktikum 14 - Sistem Autentikasi dan Proteksi Route/src/pages/profile/index.tsx
+++ Praktikum 14 - Sistem Autentikasi dan Proteksi Route/src/pages/profile/index.tsx
@@ -1,8 +1,13 @@
-export default function Profile() {
+import { useSession } from "next-auth/react";
+
+const HalamanProfile = () => {
+  const {data}: any = useSession();
   return (
     <div>
-      <h1>Halaman Profile</h1>
-      <p>Ini adalah halaman utama profil pengguna.</p>
+      <h1>Halaman Profile</h1><br />
+      <h1>Selamat Datang {data?.user?.fullname}</h1>
     </div>
   );
-}
+};
+
+export default HalamanProfile;
