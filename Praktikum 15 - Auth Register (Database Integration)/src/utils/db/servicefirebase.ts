0c7c11bfcc5eec9ceefe00315d7d803fc363d06f
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2026-04-03 12:47:12 +0700

    Bagian 2 – Membuat API Register

--- Praktikum 15 - Auth Register (Database Integration)/src/utils/db/servicefirebase.ts
+++ Praktikum 15 - Auth Register (Database Integration)/src/utils/db/servicefirebase.ts
@@ -1,4 +1,14 @@
-import { getFirestore, collection, getDocs, Firestore, getDoc, doc } from "firebase/firestore";
+import { 
+  getFirestore,
+   collection,
+    getDocs,
+    Firestore,
+    getDoc,
+    doc,
+    query,
+    addDoc,
+    where, 
+} from "firebase/firestore";
 import app from "./firebase";
 
 const db = getFirestore(app);
@@ -16,4 +26,40 @@ export async function retrieveDataByID(collectionName: string, id: string) {
   const snapshot = await getDoc(doc(db, collectionName, id));
   const data = snapshot.data();
   return data;
+}
+
+export async function signUp(
+  userData: {
+    email: string;
+    fullname: string;
+    password: string;
+  },
+  callback: Function,
+) {
+  const q = query(
+    collection(db, "users"),
+    where("email", "==", userData.email),
+  );
+  const querySnapshot = await getDocs(q);
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  // Jika data.length === 0, berarti email belum dipakai -> Boleh daftar
+  if (data.length === 0) {
+    // Jangan lupa hilangkan comment pada addDoc agar data tersimpan!
+    await addDoc(collection(db, "users"), userData);
+    
+    callback({
+      status: "success",
+      message: "User registered successfully",
+    });
+  } else {
+    // Jika email sudah ada di database -> Error
+    callback({
+      status: "error",
+      message: "User already exists",
+    });
+  }
 }
