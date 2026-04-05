e1c4a183415893e19a43fd59ab45498f416dc396
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2026-04-06 01:47:28 +0700

    Tugas Mandiri 2. Buat halaman khusus editor

---
+++ Praktikum 17 - Implementasi Login Google Provider dengan NextAuth.js + Firebase/src/pages/editor/index.tsx
@@ -0,0 +1,18 @@
+const HalamanEditor = () => {
+  return (
+    <div>
+      <div className="Editor">
+        <h1>Halaman Editor</h1>
+        <p>
+          Selamat datang di halaman Editor! Anda memiliki akses editor ke semua
+          fitur dan data di aplikasi ini. Di sini, Anda dapat mengelola
+          pengguna, melihat laporan, dan melakukan tugas Editor lainnya.
+          Pastikan untuk menggunakan hak akses Anda dengan bijak dan menjaga
+          keamanan data pengguna.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HalamanEditor;
