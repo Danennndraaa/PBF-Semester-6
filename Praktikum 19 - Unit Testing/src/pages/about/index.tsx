ea985c50fe82531de515c380c51d69fa9cb0f363
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2026-04-06 23:11:21 +0700

    PRAKTIKUM 6 – Testing dengan getByTestId

--- Praktikum 19 - Unit Testing/src/pages/about/index.tsx
+++ Praktikum 19 - Unit Testing/src/pages/about/index.tsx
@@ -1,17 +1,9 @@
-export default function About() {
+const AboutPage = () => {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        backgroundColor: "#ffffff",
-        color: "#000000",
-        padding: "40px",
-        fontFamily: "Arial, sans-serif",
-      }}
-    >
-      <h1 style={{ fontSize: "32px", fontWeight: "bold" }}>
-        Ini Halaman About
-      </h1>
+    <div>
+      <h1 data-testid="title">About Page</h1>
     </div>
   );
-}
+};
+
+export default AboutPage;
